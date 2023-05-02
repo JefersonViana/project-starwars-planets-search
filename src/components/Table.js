cf2c0036e56eff89626ba020251091f73d8de343
jeferson-viana <jefersonv28@gmail.com>
2023-05-02 15:47:14 -0300

    Requisito 1 concluído

---
+++ src/components/Table.js
@@ -0,0 +1,54 @@
+import { useContext } from 'react';
+import { AppContext } from '../context/AppProvider';
+
+function Table() {
+  const { planets } = useContext(AppContext);
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>Url</th>
+        </tr>
+      </thead>
+      <tbody>
+        {planets.map((planet) => (
+          <tr key={ planet.name }>
+            <th>{planet.name}</th>
+            <th>{planet.rotation}</th>
+            <th>{planet.orbital}</th>
+            <th>{planet.diameter}</th>
+            <th>{planet.climate}</th>
+            <th>{planet.gravity}</th>
+            <th>{planet.terrain}</th>
+            <th>{planet.surface}</th>
+            <th>{planet.population}</th>
+            <th>
+              {planet.films.map((film, index) => (
+                <ul key={ index }>
+                  <li>{film}</li>
+                </ul>
+              ))}
+            </th>
+            <th>{planet.created}</th>
+            <th>{planet.edited}</th>
+            <th>{planet.url}</th>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
---
+++ src/context/AppProvider.js
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types';
+import React, { useState, useMemo, createContext, useCallback, useEffect } from 'react';
+
+export const AppContext = createContext();
+
+function AppProvider({ children }) {
+  const [planets, setPlanets] = useState([]);
+
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await fetch('https://swapi.dev/api/planets');
+      const data = await response.json();
+      const results = data.results.map((planet) => ({
+        climate: planet.climate,
+        created: planet.created,
+        diameter: planet.diameter,
+        edited: planet.edited,
+        films: planet.films,
+        gravity: planet.gravity,
+        name: planet.name,
+        orbital: planet.orbital_period,
+        population: planet.population,
+        rotation: planet.rotation_period,
+        surface: planet.surface_water,
+        terrain: planet.terrain,
+        url: planet.url,
+      }));
+      setPlanets(results);
+    } catch (error) {
+      throw new Error();
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  const values = useMemo(() => ({
+    planets, setPlanets,
+  }), [planets, setPlanets]);
+
+  return (
+    <AppContext.Provider value={ values }>
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+AppProvider.propTypes = {
+  children: PropTypes.shape({}).isRequired,
+};
+
+export default AppProvider;
